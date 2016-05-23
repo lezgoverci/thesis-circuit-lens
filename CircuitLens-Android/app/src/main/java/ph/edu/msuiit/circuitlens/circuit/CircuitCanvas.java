aaecb52beae8c4e1d494e4cd5df1dc0fc9f4d7c4
Gilmore Capiral <eromlig.ac@gmail.com>
2016-05-23 09:42:30 +0800

    added new CircuitElem sub classes from falstad; created dummy circuit canvas

---
+++ CircuitLens-Android/app/src/main/java/ph/edu/msuiit/circuitlens/circuit/CircuitCanvas.java
@@ -0,0 +1,19 @@
+package ph.edu.msuiit.circuitlens.circuit;
+
+public class CircuitCanvas {
+    public void kill() {
+
+    }
+
+    public void init() {
+
+    }
+
+    public void repaintCanvas() {
+
+    }
+
+    public void repaintCanvas(int pause) {
+
+    }
+}
---
+++ CircuitLens-Android/app/src/main/java/ph/edu/msuiit/circuitlens/circuit/GraphicElm.java
@@ -0,0 +1,24 @@
+package ph.edu.msuiit.circuitlens.circuit;
+
+import org.rajawali3d.Object3D;
+
+public class GraphicElm extends CircuitElm {
+
+    public GraphicElm(int xx, int yy) {
+        super(xx, yy);
+    }
+
+    public GraphicElm(int xa, int ya, int xb, int yb, int flags) {
+        super(xa, ya, xb, yb, flags);
+    }
+
+    @Override
+    public int getPostCount() {
+        return 0;
+    }
+
+    @Override
+    public Object3D generateObject3D() {
+        return new Object3D();
+    }
+}

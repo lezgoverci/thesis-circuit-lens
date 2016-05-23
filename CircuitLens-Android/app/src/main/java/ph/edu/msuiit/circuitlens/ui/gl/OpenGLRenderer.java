d824ccfd9e2c4bef25297656776d2c07cbe2f59f
Gilmore Capiral <eromlig.ac@gmail.com>
2016-05-23 09:43:52 +0800

    replaced plane object with actual circuit diagram; set 3d object to continuously rotate wrt to x-axis by 3 degrees

--- CircuitLens-Android/app/src/main/java/ph/edu/msuiit/circuitlens/ui/gl/OpenGLRenderer.java
+++ CircuitLens-Android/app/src/main/java/ph/edu/msuiit/circuitlens/ui/gl/OpenGLRenderer.java
@@ -1,33 +1,34 @@
 package ph.edu.msuiit.circuitlens.ui.gl;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.util.Log;
 import android.view.MotionEvent;
 
-import org.opencv.calib3d.Calib3d;
 import org.opencv.core.Mat;
 import org.rajawali3d.Object3D;
 import org.rajawali3d.lights.DirectionalLight;
 import org.rajawali3d.materials.Material;
-import org.rajawali3d.materials.methods.DiffuseMethod;
-import org.rajawali3d.materials.textures.ATexture;
-import org.rajawali3d.materials.textures.Texture;
 import org.rajawali3d.math.vector.Vector3;
-import org.rajawali3d.primitives.Plane;
-import org.rajawali3d.primitives.Sphere;
 import org.rajawali3d.renderer.RajawaliRenderer;
 import org.rajawali3d.util.ObjectColorPicker;
 import org.rajawali3d.util.OnObjectPickedListener;
 
-import ph.edu.msuiit.circuitlens.R;
+import ph.edu.msuiit.circuitlens.circuit.CircuitElm;
+import ph.edu.msuiit.circuitlens.circuit.CircuitSimulator;
+import ph.edu.msuiit.circuitlens.circuit.elements.CapacitorElm;
+import ph.edu.msuiit.circuitlens.circuit.elements.DCVoltageElm;
+import ph.edu.msuiit.circuitlens.circuit.elements.GroundElm;
+import ph.edu.msuiit.circuitlens.circuit.elements.InductorElm;
+import ph.edu.msuiit.circuitlens.circuit.elements.ResistorElm;
+import ph.edu.msuiit.circuitlens.circuit.elements.SwitchElm;
+import ph.edu.msuiit.circuitlens.circuit.elements.WireElm;
 
-public class OpenGLRenderer  extends RajawaliRenderer implements OnObjectPickedListener {
+public class OpenGLRenderer  extends RajawaliRenderer {
 
     public Context context;
 
-    private DirectionalLight directionalLight;
-    private Plane circuitDiagram;
-    private ObjectColorPicker mPicker;
+    private Object3D circuitDiagram;
 
     public OpenGLRenderer(Context context) {
         super(context);
@@ -36,65 +37,67 @@ public class OpenGLRenderer  extends RajawaliRenderer implements OnObjectPickedL
     }
 
     public void initScene(){
-        // configure object picker
-        mPicker = new ObjectColorPicker(this);
-        mPicker.setOnObjectPickedListener(this);
-
-        //mPicker.registerObject(myObject);
-
-        directionalLight = new DirectionalLight(1f, .2f, 1.0f);
-        directionalLight.setColor(1.0f, 1.0f, 1.0f);
-        directionalLight.setPower(2);
-        getCurrentScene().addLight(directionalLight);
-
         Material material = new Material();
-        material.enableLighting(true);
-        material.setDiffuseMethod(new DiffuseMethod.Lambert());
-        material.setColor(0);
+        material.setColor(Color.WHITE);
 
-        Texture circuitTexture = new Texture("Circuit", R.drawable.circuit_a);
-        try{
-            material.addTexture(circuitTexture);
-        } catch (ATexture.TextureException error){
-            Log.d("DEBUG", "TEXTURE ERROR");
+        CircuitSimulator cirsim = new CircuitSimulator();
+        cirsim.init();
+        cirsim.register(ResistorElm.class);
+        cirsim.register(InductorElm.class);
+        cirsim.register(CapacitorElm.class);
+        cirsim.register(DCVoltageElm.class);
+        cirsim.register(WireElm.class);
+        cirsim.register(SwitchElm.class);
+        cirsim.register(DCVoltageElm.class);
+        cirsim.register(GroundElm.class);
+        final String expectedNetlist =
+                "r 176 64 384 64 0 10\n" +
+                "s 384 64 448 64 0 1 false\n" +
+                "w 176 64 176 336 0\n" +
+                "c 384 336 176 336 0 0.000014999999999999998 -0.0000020730346837365553\n" +
+                "l 384 64 384 336 0 1 -2.4257677603955542e-8\n" +
+                "v 448 336 448 64 0 0 40 5 0 0 0.5\n" +
+                "r 384 336 448 336 0 100\n";
+
+        circuitDiagram = new Object3D();
+        cirsim.readSetup(expectedNetlist);
+        for(CircuitElm elm : cirsim.elmList){
+            Object3D circuitElm3d = elm.generateObject3D();
+            circuitElm3d.setMaterial(material);
+            circuitDiagram.addChild(circuitElm3d);
+            Log.d(this.getClass().getSimpleName(),"CircuitElm: " + circuitElm3d.getX() + "," + circuitElm3d.getY());
         }
-
-        circuitDiagram = new Plane(3, 3, 1, 1);
-        circuitDiagram.setMaterial(material);
-        circuitDiagram.setTransparent(true);
-        circuitDiagram.setDoubleSided(true);
+        // invert y-axis
+        circuitDiagram.setScaleY(-1);
 
         getCurrentScene().addChild(circuitDiagram);
-        getCurrentCamera().setZ(4.2f);
+        getCurrentCamera().setZ(100);
+        getCurrentCamera().setX(300);
+        getCurrentCamera().setFarPlane(500);
+        getCurrentCamera().setY(-200);
+        getCurrentCamera().setFieldOfView(130);
     }
-
 
     @Override
     public void onRender(final long elapsedTime, final double deltaTime) {
         super.onRender(elapsedTime, deltaTime);
-        //circuitDiagram.rotate(Vector3.Axis.Y, 1.0);
-    }
-
-
-    public void onTouchEvent(MotionEvent event){
-
+        circuitDiagram.rotate(Vector3.Axis.X, 3);
     }
 
     public void onOffsetsChanged(float x, float y, float z, float w, int i, int j){
 
     }
 
+    @Override
+    public void onTouchEvent(MotionEvent event) {
+        if(event.getAction() == MotionEvent.ACTION_DOWN)
+        {
+            Log.d(this.getClass().getSimpleName(),": " + event.getX()+ "," + event.getY());
+        }
+    }
+
     public void setProjectionValues(Mat f) {
 //        Calib3d.decomposeHomographyMat(f,);
-    }
-
-    @Override
-    public void onObjectPicked(Object3D object) {
-
-    }
-
-    public void getObjectAt(float x, float y) {
-        mPicker.getObjectAt(x, y);
     }
 }
 

90ae69b08b5035159aa40890fae7e00351dcfff9
Geomart Brenth Abong <n2txheen@gmail.com>
2016-05-30 22:40:17 +0800

    Implemented match function

--- CircuitLens-Server/app/recognizer/class_features_using_recognizer_classes_db.py
+++ CircuitLens-Server/app/recognizer/class_features_using_recognizer_classes_db.py
@@ -1,7 +1,32 @@
 import class_classes_database as cdb
 import class_feature_using_recognizer as fur
+import class_fur_absolute_magnitude_difference_solver as famd
 
 class FeaturesUsingRecognizerClassesDB(cdb.ClassesDatabase):
+    def __init__(self):
+        self.__recognizer = fur.FeatureUsingRecognizer()
+        self.__featuresDistanceSolver = famd.FURAbsoluteMagnitudeDifferenceSolver()
+        
+    #-----------------------------------------
+    # Getters
+    #-----------------------------------------
+    
+    def match(self, calculatedFeature):
+        if not self._classes_values_map:
+            return None
+        
+        minClass = None
+        minMatchPercentage = float('inf')
+        
+        for className, storedFeature in self._classes_values_map.iteritems():
+            d = self.__featuresDistanceSolver.solve(storedFeature, calculatedFeature)
+            
+            if minMatchPercentage > d:
+                minMatchPercentage = d
+                minClass = className
+        
+        return minClass, minMatchPercentage
+    
     #-----------------------------------------
     # Other Functions
     #-----------------------------------------
@@ -9,10 +34,8 @@ class FeaturesUsingRecognizerClassesDB(cdb.ClassesDatabase):
     def train(self, classes_images_map):
         if not classes_images_map:
             return self
-        
-        recognizer = fur.FeatureUsingRecognizer()
-        
+
         for classStr, img in classes_images_map.iteritems():
-            self._classes_values_map[classStr] = recognizer.setImage(img).getCalculatedFeature()
+            self._classes_values_map[classStr] = self.__recognizer.setImage(img).getCalculatedFeature()
         
         return self

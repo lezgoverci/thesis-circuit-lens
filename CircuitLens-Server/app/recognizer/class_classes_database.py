6b9c7954fe14c3494e573a17f02e58fe79b15603
Geomart Brenth Abong <n2txheen@gmail.com>
2016-05-30 22:38:44 +0800

    Removed iterableness of classes db and added match function.

--- CircuitLens-Server/app/recognizer/class_classes_database.py
+++ CircuitLens-Server/app/recognizer/class_classes_database.py
@@ -1,8 +1,6 @@
 from abc import ABCMeta, abstractmethod
-import common.class_iterable as i
-import common.class_list_iterable_iterator as lii
 
-class ClassesDatabase(i.Iterable):
+class ClassesDatabase():
     
     def __init__(self):
         self._classes_values_map = {}
@@ -10,25 +8,14 @@ class ClassesDatabase(i.Iterable):
     #-----------------------------------------
     # Getters
     #-----------------------------------------
-
-    def getData(self, n):
-        if not self.accessible():
-            raise LookupError
-        
-        return self._classes_values_map[n]
     
-    def getIterator(self):
-        return lii.ListIterableIterator(self)
-    
-    def size(self):
-        return len(self._classes_values_map)
+    @abstractmethod
+    def match(self, calculatedValue):
+        pass
     
     #-----------------------------------------
     # Other Functions
     #----------------------------------------- 
-    
-    def accessible(self, n):
-        return None == self._classes_values_map.get(n, None)
     
     def clear(self):
         self._classes_values_map = {}

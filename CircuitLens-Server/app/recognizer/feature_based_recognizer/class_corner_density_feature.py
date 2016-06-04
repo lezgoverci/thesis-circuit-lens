Done minor bugs fixes. Changed some of features calculation to be scale invariant. Added data extractors to factories.

CircuitLens-Server/app/recognizer/feature_based_recognizer/class_corner_density_feature.py
@@ -6,7 +6,7 @@ class CornerDensityFeature(f.Feature):
     def __init__(self):
         self.__arguments = None
         self.__neededArguments = ['area', 'centroid', 'img', 'feature_data_extractors']
-        self.__neededFeatureDataExtractors = ['keypoints']
+        self.__neededFeatureDataExtractors = ['corners_keypoints', 'central_angles']
         self.__calculatedFeature = None
     
     #-----------------------------------------
@@ -23,8 +23,8 @@ class CornerDensityFeature(f.Feature):
     #-----------------------------------------
     
     def getCalculatedFeature(self, recalculate=False):
-        if not self.__calculatedFeature or recalculate:
-            self.calculate()
+        if self.__calculatedFeature is None or recalculate:
+            self.calculate(True)
         
         return self.__calculatedFeature
     
@@ -35,24 +35,28 @@ class CornerDensityFeature(f.Feature):
     # Other Functions
     #-----------------------------------------
 
-    def calculate(self):
+    def calculate(self, recalculate=False):
         if not self.argumentsMet():
             return None
+
+        centralAnglesExtractor = self.__arguments['feature_data_extractors']['central_angles']
+        keyPointsExtractor = self.__arguments['feature_data_extractors']['corners_keypoints']
+
+        keyPointsExtractor.setArguments({
+            'centroid': self.__arguments['centroid'],
+            'img': self.__arguments['img']
+        })
         
-        if 0 == self.__arguments['area']:
-            return np.array([0.0, 0.0, 0.0])
+        corners, _ = keyPointsExtractor.getExtractedData(recalculate)
         
-        keyPointsExtractor = self.__arguments['feature_data_extractors']['keypoints']
+        centralAnglesExtractor.setArguments({
+            'corners': corners,
+            'centroid': self.__arguments['centroid'] 
+        })
         
-        if not keyPointsExtractor.argumentsMet():
-            keyPointsExtractor.setArguments({
-                'centroid': self.__arguments['centroid'],
-                'img': self.__arguments['img']
-            })
+        centralAngles, angleVectorMap = centralAnglesExtractor.getExtractedData(recalculate)
         
-        corners, _ = keyPointsExtractor.getExtractedData()
-        
-        self.__calculatedFeature = np.array([len(corners) * 10 / self.__arguments['area'], 0.0, 0.0])
+        self.__calculatedFeature = np.array([(len(centralAngles)) / self.__arguments['area'], 0.0, 0.0])
         
         return self
 

CircuitLens-Server/app/recognizer/feature_based_recognizer/class_corner_distance_feature.py
@@ -6,7 +6,7 @@ class CornerDistanceFeature(f.Feature):
     def __init__(self):
         self.__arguments = None
         self.__neededArguments = ['area', 'centroid', 'img', 'feature_data_extractors']
-        self.__neededFeatureDataExtractors = ['central_angles', 'keypoints']
+        self.__neededFeatureDataExtractors = ['central_angles', 'corners_keypoints']
         self.__calculatedFeature = None
     
     #-----------------------------------------
@@ -23,8 +23,8 @@ class CornerDistanceFeature(f.Feature):
     #-----------------------------------------
     
     def getCalculatedFeature(self, recalculate=False):
-        if not self.__calculatedFeature or recalculate:
-            self.calculate()
+        if self.__calculatedFeature is None or recalculate:
+            self.calculate(True)
         
         return self.__calculatedFeature
     
@@ -35,7 +35,7 @@ class CornerDistanceFeature(f.Feature):
     # Other Functions
     #-----------------------------------------
 
-    def calculate(self):
+    def calculate(self, recalculate=False):
         if not self.argumentsMet():
             return None
         
@@ -43,23 +43,21 @@ class CornerDistanceFeature(f.Feature):
             return np.array([0.0, 0.0, 0.0])
         
         centralAnglesExtractor = self.__arguments['feature_data_extractors']['central_angles']
-        keyPointsExtractor = self.__arguments['feature_data_extractors']['keypoints']
+        keyPointsExtractor = self.__arguments['feature_data_extractors']['corners_keypoints']
         
-        if not keyPointsExtractor.argumentsMet():
-            keyPointsExtractor.setArguments({
-                'centroid': self.__arguments['centroid'],
-                'img': self.__arguments['img']
-            })
+        keyPointsExtractor.setArguments({
+            'centroid': self.__arguments['centroid'],
+            'img': self.__arguments['img']
+        })
         
-        corners, _ = keyPointsExtractor.getExtractedData()
+        corners, _ = keyPointsExtractor.getExtractedData(recalculate)
+
+        centralAnglesExtractor.setArguments({
+            'corners': corners,
+            'centroid': self.__arguments['centroid'] 
+        })
         
-        if not centralAnglesExtractor.argumentsMet():
-            centralAnglesExtractor.setArguments({
-                'corners': corners,
-                'centroid': self.__arguments['centroid'] 
-            })
-        
-        centralAngles, angleVectorMap = centralAnglesExtractor.getExtractedData()
+        centralAngles, angleVectorMap = centralAnglesExtractor.getExtractedData(recalculate)
 
         cornerDistance = np.array([0.0, 0.0, 0.0])
     
@@ -69,7 +67,7 @@ class CornerDistanceFeature(f.Feature):
         while i < len(centralAngles):
             try:
                 currentVector = angleVectorMap[centralAngles[i]]
-                cornerDistance += (currentVector - prevVector) * 10 / self.__arguments['area']
+                cornerDistance += (currentVector - prevVector) 
                 
             except Exception as e:
                 print e
@@ -78,7 +76,7 @@ class CornerDistanceFeature(f.Feature):
             
             i += 1
         
-        self.__calculatedFeature = cornerDistance
+        self.__calculatedFeature = (cornerDistance * 10) / self.__arguments['area']
         
         return self
 

CircuitLens-Server/app/recognizer/feature_based_recognizer/class_disperseness_from_centroid_feature.py
@@ -7,7 +7,7 @@ class DispersenessFromCentroidFeature(f.Feature):
     def __init__(self):
         self.__arguments = None
         self.__neededArguments = ['area', 'centroid', 'img', 'feature_data_extractors']
-        self.__neededFeatureDataExtractors = ['central_angles', 'keypoints']
+        self.__neededFeatureDataExtractors = ['central_angles', 'edges_keypoints']
         self.__calculatedFeature = None
     
     #-----------------------------------------
@@ -24,8 +24,8 @@ class DispersenessFromCentroidFeature(f.Feature):
     #-----------------------------------------
     
     def getCalculatedFeature(self, recalculate=False):
-        if not self.__calculatedFeature or recalculate:
-            self.calculate()
+        if self.__calculatedFeature is None or recalculate:
+            self.calculate(True)
         
         return self.__calculatedFeature
     
@@ -36,7 +36,7 @@ class DispersenessFromCentroidFeature(f.Feature):
     # Other Functions
     #-----------------------------------------
 
-    def calculate(self):
+    def calculate(self, recalculate=False):
         if not self.argumentsMet():
             return None
             
@@ -44,23 +44,21 @@ class DispersenessFromCentroidFeature(f.Feature):
             return np.array([0.0, 0.0, 0.0])
         
         centralAnglesExtractor = self.__arguments['feature_data_extractors']['central_angles']
-        keyPointsExtractor = self.__arguments['feature_data_extractors']['keypoints']
+        keyPointsExtractor = self.__arguments['feature_data_extractors']['edges_keypoints']
         
-        if not keyPointsExtractor.argumentsMet():
-            keyPointsExtractor.setArguments({
-                'centroid': self.__arguments['centroid'],
-                'img': self.__arguments['img']
-            })
+        keyPointsExtractor.setArguments({
+            'centroid': self.__arguments['centroid'],
+            'img': self.__arguments['img']
+        })
         
-        corners, _ = keyPointsExtractor.getExtractedData()
+        corners, _ = keyPointsExtractor.getExtractedData(recalculate)
+
+        centralAnglesExtractor.setArguments({
+            'corners': corners,
+            'centroid': self.__arguments['centroid'] 
+        })
         
-        if not centralAnglesExtractor.argumentsMet():
-            centralAnglesExtractor.setArguments({
-                'corners': corners,
-                'centroid': self.__arguments['centroid'] 
-            })
-        
-        centralAngles, angleVectorMap = centralAnglesExtractor.getExtractedData()
+        centralAngles, angleVectorMap = centralAnglesExtractor.getExtractedData(recalculate)
 
         dispersenessFromCentroid = 0
 
@@ -71,14 +69,14 @@ class DispersenessFromCentroidFeature(f.Feature):
             try:
                 currentVector = angleVectorMap[centralAngles[i]]
 
-                dispersenessFromCentroid += currentVector / self.__arguments['area']
+                dispersenessFromCentroid += currentVector
                 
             except Exception as e:
                 print e
 
             i += 1
 
-        self.__calculatedFeature = dispersenessFromCentroid
+        self.__calculatedFeature = dispersenessFromCentroid / self.__arguments['area']
         
         return self
 

CircuitLens-Server/app/recognizer/feature_based_recognizer/class_features_using_recognizer_classes_db.py
@@ -7,8 +7,8 @@ import numpy as np
 class FeaturesUsingRecognizerClassesDB(cdb.ClassesDatabase):
     def __init__(self):
         super(FeaturesUsingRecognizerClassesDB, self).__init__()
-        # self.__featuresDistanceSolver = famd.FURAbsoluteMagnitudeDifferenceSolver()
-        self.__featuresDistanceSolver = ffds.FURFeatureDifferenceSolver()
+        self.__featuresDistanceSolver = famd.FURAbsoluteMagnitudeDifferenceSolver()
+        # self.__featuresDistanceSolver = ffds.FURFeatureDifferenceSolver()
         
     #-----------------------------------------
     # Getters
@@ -46,8 +46,12 @@ class FeaturesUsingRecognizerClassesDB(cdb.ClassesDatabase):
         minClass = None
         minMatchPercentage = float('inf')
         
+        self.__print('Query Image', calculatedFeature)
+        
         for className, (storedFeature, _) in self._classesValuesMap.iteritems():
             d = self.__featuresDistanceSolver.solve(storedFeature, calculatedFeature)
+            
+            self.__print(className, storedFeature)
             
             if minMatchPercentage > d:
                 minMatchPercentage = d
@@ -63,8 +67,15 @@ class FeaturesUsingRecognizerClassesDB(cdb.ClassesDatabase):
         recognizer = fur.FeaturesUsingRecognizer()
         
         for classStr, img in classesImagesMap.iteritems():
+            print "Calculating %s features" % (classStr)
             self._classesValuesMap[classStr] = (recognizer.setImage(img).getCalculatedFeature(True), img.shape)
         
         return self
+    
+    def __print(self, label, feature):
+        print 'label: ' + label
+        print 'raw value: \n' + str(feature)
+        print 'magnitude: ' + str(np.linalg.norm(feature))
+        print '---------------------------------------'
 
 instance = FeaturesUsingRecognizerClassesDB()

CircuitLens-Server/app/recognizer/feature_based_recognizer/class_gearness_feature.py
@@ -6,8 +6,8 @@ import class_feature_processable_data_extractor as fpde
 class GearnessFeature(f.Feature):
     def __init__(self):
         self.__arguments = None
-        self.__neededArguments = ['centroid', 'img', 'feature_data_extractors']
-        self.__neededFeatureDataExtractors = ['central_angles', 'keypoints']
+        self.__neededArguments = ['area', 'centroid', 'img', 'feature_data_extractors']
+        self.__neededFeatureDataExtractors = ['central_angles', 'corners_keypoints']
         self.__calculatedFeature = None
     
     #-----------------------------------------
@@ -24,8 +24,8 @@ class GearnessFeature(f.Feature):
     #-----------------------------------------
     
     def getCalculatedFeature(self, recalculate=False):
-        if not self.__calculatedFeature or recalculate:
-            self.calculate()
+        if self.__calculatedFeature is None or recalculate:
+            self.calculate(True)
         
         return self.__calculatedFeature
     
@@ -36,28 +36,26 @@ class GearnessFeature(f.Feature):
     # Other Functions
     #-----------------------------------------
 
-    def calculate(self):
+    def calculate(self, recalculate=False):
         if not self.argumentsMet():
             return None
         
         centralAnglesExtractor = self.__arguments['feature_data_extractors']['central_angles']
-        keyPointsExtractor = self.__arguments['feature_data_extractors']['keypoints']
+        keyPointsExtractor = self.__arguments['feature_data_extractors']['corners_keypoints']
         
-        if not keyPointsExtractor.argumentsMet():
-            keyPointsExtractor.setArguments({
-                'centroid': self.__arguments['centroid'],
-                'img': self.__arguments['img']
-            })
+        keyPointsExtractor.setArguments({
+            'centroid': self.__arguments['centroid'],
+            'img': self.__arguments['img']
+        })
         
-        corners, _ = keyPointsExtractor.getExtractedData()
+        corners, _ = keyPointsExtractor.getExtractedData(recalculate)
         
-        if not centralAnglesExtractor.argumentsMet():
-            centralAnglesExtractor.setArguments({
-                'corners': corners,
-                'centroid': self.__arguments['centroid'] 
-            })
+        centralAnglesExtractor.setArguments({
+            'corners': corners,
+            'centroid': self.__arguments['centroid'] 
+        })
         
-        centralAngles, angleVectorMap = centralAnglesExtractor.getExtractedData()
+        centralAngles, angleVectorMap = centralAnglesExtractor.getExtractedData(recalculate)
 
         gearness = np.array([0.0, 0.0, 0.0])
     
@@ -74,10 +72,7 @@ class GearnessFeature(f.Feature):
                 rB = prevVector - currentVector
                 rA = nextVector - currentVector
 
-                normalizer = bf.BasicFunctions.calculatePointsDistance(rB, rA)
-
-                if 0 != normalizer:
-                    gearness += np.cross(rB, rA) / normalizer
+                gearness += np.cross(rB, rA)
                 
             except Exception as e:
                 print e
@@ -86,7 +81,7 @@ class GearnessFeature(f.Feature):
             
             i += 1
         
-        self.__calculatedFeature = gearness
+        self.__calculatedFeature = gearness / self.__arguments['area']
         
         return self
 

CircuitLens-Server/app/recognizer/feature_based_recognizer/class_hu_moments_feature.py
@@ -25,8 +25,8 @@ class HuMomentsFeature(f.Feature):
     #-----------------------------------------
     
     def getCalculatedFeature(self, recalculate=False):
-        if not self.__calculatedFeature or recalculate:
-            self.calculate()
+        if self.__calculatedFeature is None or recalculate:
+            self.calculate(True)
         
         return self.__calculatedFeature
     
@@ -37,12 +37,14 @@ class HuMomentsFeature(f.Feature):
     # Other Functions
     #-----------------------------------------
 
-    def calculate(self):
+    def calculate(self, recalculate=False):
         if not self.argumentsMet():
             return None
         
         # self.__calculatedFeature = cv2.HuMoments(self.__arguments['moments']).flatten()
-        self.__calculatedFeature = np.array([np.linalg.norm(cv2.HuMoments(self.__arguments['moments']).flatten()), 0.0, 0.0])
+        rawHuMoments = cv2.HuMoments(self.__arguments['moments']).flatten()[:6]
+        # self.__calculatedFeature = np.array([np.linalg.norm(), 0.0, 0.0])
+        self.__calculatedFeature = np.array([np.linalg.norm(rawHuMoments), 0.0, 0.0])
         
         return self
 
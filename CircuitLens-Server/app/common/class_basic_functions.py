82f86187fbc073d5920a52f36b71653451a5f0c6
Geomart Brenth Abong <n2txheen@gmail.com>
2016-06-04 08:55:25 +0800

    Added new functions.

--- CircuitLens-Server/app/common/class_basic_functions.py
+++ CircuitLens-Server/app/common/class_basic_functions.py
@@ -18,7 +18,7 @@ class BasicFunctions:
     @staticmethod
     def lineIntersection(line1, line2):
         xdiff = (line1[0][0] - line1[1][0], line2[0][0] - line2[1][0])
-        ydiff = (line1[0][1] - line1[1][1], line2[0][1] - line2[1][1]) #Typo was here
+        ydiff = (line1[0][1] - line1[1][1], line2[0][1] - line2[1][1])
 
         def det(a, b):
             return a[0] * b[1] - a[1] * b[0]
@@ -31,7 +31,7 @@ class BasicFunctions:
         x = det(d, xdiff) / div
         y = det(d, ydiff) / div
         
-        return np.array([x, y])
+        return np.array([x, y, 0.0])
     
     @staticmethod
     def calculateAngle(a, b, directionMatters=True):
@@ -50,7 +50,7 @@ class BasicFunctions:
         l1 = ((a + b) / 2, c)
         l2 = ((b + c) / 2, a)
 
-        return lineIntersection(l1, l2)
+        return BasicFunctions.lineIntersection(l1, l2)
     
     @staticmethod
     def calculatePointsDistance(a, b):
@@ -86,7 +86,7 @@ class BasicFunctions:
             while x < w:
                 if 0 != img[y][x] and x != xbar and y != ybar:
                     v = np.array([float(x - xbar), float(y - ybar)])
-                    xsub, ysub = calculateUnitVector(v)
+                    xsub, ysub = BasicFunctions.calculateUnitVector(v)
                     
                     xs += xsub
                     ys += ysub
@@ -98,3 +98,43 @@ class BasicFunctions:
     @staticmethod
     def loadImage(img):
         return cv2.bitwise_not(cv2.imread(img, cv2.IMREAD_GRAYSCALE))
+    
+    @staticmethod
+    def getBorders(center, half_size, (h, w)):
+        base_x = [center[0] - half_size, center[0] + half_size]
+        base_y = [center[1] - half_size, center[1] + half_size]
+        
+        base_x[0] = base_x[0] if base_x[0] >= 0 else 0
+        base_x[1] = base_x[1] if base_x[1] < w else w - 1
+        
+        base_y[0] = base_y[0] if base_y[0] >= 0 else 0
+        base_y[1] = base_y[1] if base_y[1] < h else h - 1
+        
+        return [
+            ([base_x[0], base_y[0]], [base_x[1], base_y[0]], (base_x[0], base_y[0] + 1), (1, 0)), #top
+            ([base_x[1], base_y[0] + 1], [base_x[1], base_y[1] - 1], (base_x[1], base_y[0]), (0, 1)), #right
+            ([base_x[1], base_y[1]], [base_x[0], base_y[1]], (base_x[1], base_y[1] - 1), (-1, 0)), #bottom
+            ([base_x[0], base_y[1] - 1], [base_x[0], base_y[0] + 1], (base_x[0], base_y[1]), (0, -1)), #left
+        ]
+    
+    @staticmethod
+    def removeFourConnectedness(matrix):
+        h, w = matrix.shape[:2]
+        
+        y = 0
+        cleared_matrix = matrix.copy()
+        while y < h:
+            prev_y = y - 1
+            x = 0
+            while x < w:
+                if 255 == matrix[y][x]:
+                    next_x = x + 1
+                    prev_x = x - 1
+                    if prev_y >=0 and prev_x >= 0 and 255 == matrix[prev_y][x] and 255 == matrix[y][prev_x]:
+                        cleared_matrix[y][x] = 0
+                    elif prev_y >= 0 and next_x < w and 255 == matrix[prev_y][x] and 255 == matrix[y][next_x]:
+                        cleared_matrix[y][x] = 0
+                x += 1
+            y += 1
+        
+        return cleared_matrix

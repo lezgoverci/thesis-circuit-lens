7ea22375a58f077530a4a415a52e6be734ffc089
vercillius <vercilliusjrmila@gmail.com>
2016-06-10 11:30:30 +0800

    deleted unnecessary classes

--- CircuitLens-Android/app/src/test/java/ph/edu/msuiit/circuitlens/OverlayImageTransformationMapperTest.java
+++ CircuitLens-Android/app/src/test/java/ph/edu/msuiit/circuitlens/OverlayImageTransformationMapperTest.java
@@ -1,9 +1,6 @@
 package ph.edu.msuiit.circuitlens;
 
-import org.junit.Test;
 import org.opencv.core.Mat;
-
-import ph.edu.msuiit.circuitlens.render.OverlayImageTransformationMapper;
 
 import static junit.framework.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
@@ -16,13 +13,7 @@ public class OverlayImageTransformationMapperTest {
 
     final Mat trackingImg = null;
     OverlayImage overlayImg = new AnimatedOverlayImage();
-    OverlayImageTransformationMapper mapper;
 
-    @Test
-    public void testOverlay(){
-        mapper = new OverlayImageTransformationMapper();
-        assertTrue(true);
-    }
 
 
 }

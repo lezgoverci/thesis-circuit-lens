66a0ac5d5756a57d7335665d5b787802302815a1
Gilmore Capiral <eromlig.ac@gmail.com>
2016-06-12 11:36:09 +0800

    created tests for luFactor and luSolve functions

--- CircuitLens-Android/app/src/test/java/ph/edu/msuiit/circuitlens/CircuitSimulatorTest.java
+++ CircuitLens-Android/app/src/test/java/ph/edu/msuiit/circuitlens/CircuitSimulatorTest.java
@@ -2,28 +2,10 @@ package ph.edu.msuiit.circuitlens;
 
 import org.junit.Test;
 
-import java.util.concurrent.TimeUnit;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
 
 import ph.edu.msuiit.circuitlens.circuit.CircuitSimulator;
-import ph.edu.msuiit.circuitlens.circuit.Scope;
-import ph.edu.msuiit.circuitlens.circuit.elements.CapacitorElm;
-import ph.edu.msuiit.circuitlens.circuit.elements.DCVoltageElm;
-import ph.edu.msuiit.circuitlens.circuit.elements.InductorElm;
-import ph.edu.msuiit.circuitlens.circuit.elements.OutputElm;
-import ph.edu.msuiit.circuitlens.circuit.elements.ProbeElm;
-import ph.edu.msuiit.circuitlens.circuit.elements.ResistorElm;
-import rx.Observable;
-import rx.Observer;
-import rx.Subscription;
-import rx.functions.Action1;
-import ws.wamp.jawampa.ApplicationError;
-import ws.wamp.jawampa.Request;
-import ws.wamp.jawampa.WampClient;
-import ws.wamp.jawampa.WampClientBuilder;
-import ws.wamp.jawampa.WampError;
-import ws.wamp.jawampa.connection.IWampConnectorProvider;
-import ws.wamp.jawampa.internal.SingleThreadedComputationScheduler;
-import ws.wamp.jawampa.transport.netty.NettyWampClientConnectorProvider;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
@@ -64,5 +46,157 @@ public class CircuitSimulatorTest {
                 "o 2 64 0 35 40.0 0.2 0 -1\n";
         cirsim.readSetup(expectedNetlist);
         cirsim.runCircuit();
+    }
+
+    // A = LU
+    // | 8  2  9 |   | 1     0       0 |  | 8  2  9     |
+    // | 4  9  4 | = | 1/2   1       0 |  | 0  8 -1/2   |
+    // | 6  7  9 |   | 3/4   11/16  1  |  | 0  0  83/32 |
+    @Test
+    public void testLuFactor() throws Exception {
+        double[][] a = new double[][]{
+                {  8, 2, 9},
+                {  4, 9, 4},
+                {  6, 7, 9}
+        };
+        double[][] lu = new double[][]{
+                {  8,    2,         9       },
+                {  0.5,  8,        -0.5     },
+                {  0.75, 11./16.,   83./32. }
+        };
+        int ipvt[] = new int[3];
+        int expectedIpvt[] = new int[] { 0, 1, 2};
+        boolean result = CircuitSimulator.luFactor(a,3,ipvt);
+
+        assertThat(result, is(true));
+        assertThat(a, is(lu));
+        assertThat(ipvt, is(expectedIpvt));
+    }
+
+    // A = LU
+    // | 0  5  5 |   | 1        0       0 |  | 6  8       8      |
+    // | 2  9  0 | = | 1/3      1       0 |  | 0  19/13  -8/3    |
+    // | 6  8  8 |   | 0        15/19   1 |  | 0  0       135/19 |
+    @Test
+    public void testLuFactor2() throws Exception {
+        double[][] a = new double[][]{
+                {  0, 5, 5},
+                {  2, 9, 0},
+                {  6, 8, 8}
+        };
+        double[][] lu = new double[][]{
+                {  6,      8,         8        },
+                {  1./3.,  19./3.,   -8./3.    },
+                {  0,      15./19.,   135./19. }
+        };
+        int ipvt[] = new int[3];
+        int expectedIpvt[] = new int[] { 2, 1, 2};
+        boolean success = CircuitSimulator.luFactor(a,3,ipvt);
+
+        assertThat(success, is(true));
+        for(int i=0;i<3;i++)
+            for(int j=0;j<3;j++)
+                assertTrue(closeTo(a[i][j], lu[i][j], 0.001));
+        assertThat(ipvt, is(expectedIpvt));
+    }
+
+    @Test
+    public void testLuFactorZeroMatrix() throws Exception {
+        double[][] a = new double[][]{
+                {  0, 0, 0},
+                {  0, 0, 0},
+                {  0, 0, 0}
+        };
+        int ipvt[] = new int[3];
+        boolean success = CircuitSimulator.luFactor(a,3,ipvt);
+        assertThat(success, is(false));
+    }
+
+    // Ax = b
+    // A = LU
+    //
+    // L = | 1  0  |  U = | 2  1 |  b = | 11 |
+    //     |-1  1  |      | 0  9 |      |  9 |
+    //
+    // LY = b; UX = Y
+    // solution [ 2, 5 ]
+    @Test
+    public void testLuSolve() throws Exception {
+        double[][] lu = new double[][]{
+                {  2, 1},
+                { -1, 4}
+        };
+        double[] b = new double[]{ 11, 9};
+        double[] solution = new double[]{ 2, 5};
+        int ipvt[] = new int[]{ 1, 1};
+
+        CircuitSimulator.luSolve(lu, 2, ipvt, b);
+        for(int i=0;i<b.length;i++)
+            assertTrue(closeTo(b[i], solution[i], 0.001));
+    }
+
+    // A = LU
+    // | 0  5  5 |   | 1        0       0 |  | 6  8       8      |
+    // | 2  9  0 | = | 1/3      1       0 |  | 0  19/13  -8/3    |
+    // | 6  8  8 |   | 0        15/19   1 |  | 0  0       135/19 |
+    @Test
+    public void testLuSolve2() throws Exception {
+        double[][] lu = new double[][]{
+                {  6,      8,         8        },
+                {  1./3.,  19./3.,   -8./3.    },
+                {  0,      15./19.,   135./19. }
+        };
+        double[] b = new double[]{ 1, 2, 3};
+        double[] solution = new double[]{ 7./30., 23./135., 4./135.};
+        int ipvt[] = new int[]{ 2, 1, 2};
+
+        CircuitSimulator.luSolve(lu,3,ipvt,b);
+        assertThat(b, is(solution));
+    }
+
+    /*
+     *  Helper function for testing similar values
+     */
+    private boolean closeTo(double actual, double expected, double error) {
+        if(expected == 0){
+            return Math.abs(actual-expected) <= error;
+        }
+        return Math.abs((actual-expected)/expected) <= error;
+    }
+
+    private String arrayToString(double[][] a){
+        NumberFormat numberFormat = new DecimalFormat("###,###.####");
+        numberFormat.setMaximumFractionDigits(4);
+        StringBuilder sb = new StringBuilder();
+        for(int i=0;i<a.length;i++){
+            for (int j=0;j<a[0].length;j++){
+                sb.append(numberFormat.format(a[i][j]));
+                sb.append("\t");
+            }
+            sb.append("\n");
+        }
+        return sb.toString();
+    }
+
+    private String arrayToString(int[] a){
+        NumberFormat numberFormat = new DecimalFormat("###,###.####");
+        numberFormat.setMaximumFractionDigits(4);
+        StringBuilder sb = new StringBuilder();
+        for(int i=0;i<a.length;i++){
+            sb.append(numberFormat.format(a[i]));
+            sb.append("\t");
+        }
+        return sb.toString();
+    }
+
+    private String arrayToString(double[] a){
+        NumberFormat numberFormat = new DecimalFormat("###,###.####");
+        numberFormat.setMaximumFractionDigits(4);
+        StringBuilder sb = new StringBuilder();
+        for(int i=0;i<a.length;i++){
+            sb.append(numberFormat.format(a[i]));
+            sb.append("\t");
+        }
+        return sb.toString();
     }
 }

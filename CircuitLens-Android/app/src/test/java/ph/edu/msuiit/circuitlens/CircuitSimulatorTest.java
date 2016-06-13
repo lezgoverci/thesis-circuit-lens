cf19279e9e6768f4998ae1614ce9a2f20a44e2f7
Gilmore Capiral <eromlig.ac@gmail.com>
2016-06-13 10:18:19 +0800

    removed testRunCircuit test; removed scope in expectedNetlist

--- CircuitLens-Android/app/src/test/java/ph/edu/msuiit/circuitlens/CircuitSimulatorTest.java
+++ CircuitLens-Android/app/src/test/java/ph/edu/msuiit/circuitlens/CircuitSimulatorTest.java
@@ -27,25 +27,11 @@ public class CircuitSimulatorTest {
                 "v 112 240 112 160 0 1 35.0 5.0 0.0 0.0 0.5\n" +
                 "r 112 160 464 160 0 10.0\n" +
                 "c 464 160 464 240 0 1.5E-5 17.99235566721663\n" +
-                "l 112 240 464 240 0 1.0 0.0082376017397377\n" +
-                "o 2 64 0 35 40.0 0.2 0 -1\n";
+                "l 112 240 464 240 0 1.0 0.0082376017397377\n";
         cirsim.init();
         cirsim.readSetup(expectedNetlist);
         cirsim.updateCircuit();
         assertThat(cirsim.dumpCircuit(),is(expectedNetlist));
-    }
-
-    @Test
-    public void testRunCircuit() throws Exception {
-        CircuitSimulator cirsim = new CircuitSimulator();
-        final String expectedNetlist = "$ 1 5.0E-6 14.841315910257661 48.0 5.0 50.0\n" +
-                "v 112 240 112 160 0 1 35.0 5.0 0.0 0.0 0.5\n" +
-                "r 112 160 464 160 0 10.0\n" +
-                "c 464 160 464 240 0 1.5E-5 17.99235566721663\n" +
-                "l 112 240 464 240 0 1.0 0.0082376017397377\n" +
-                "o 2 64 0 35 40.0 0.2 0 -1\n";
-        cirsim.readSetup(expectedNetlist);
-        cirsim.runCircuit();
     }
 
     // A = LU
